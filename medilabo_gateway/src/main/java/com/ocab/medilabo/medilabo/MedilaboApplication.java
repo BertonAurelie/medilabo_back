66042b227a37ab7292e1968e30377b60a51e5d20
AB <ab>
2025-10-30 18:10:40 +0100

    add patient gateway routes

---
+++ medilabo_gateway/src/main/java/com/ocab/medilabo/medilabo/MedilaboApplication.java
@@ -0,0 +1,40 @@
+package com.ocab.medilabo.medilabo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.*;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+
+@SpringBootApplication
+public class MedilaboApplication {
+	private static final String PATH_PATIENT_URI = "http://localhost:8081";
+	@Bean
+	public RouterFunction<ServerResponse> myRoutes() {
+		return route("list_of_patients")
+					.GET("/patient", http())
+					.before(uri(PATH_PATIENT_URI))
+					.build()
+				.and(route("patient_delete")
+						.GET("/patient/{id}", http())
+						.before(uri(PATH_PATIENT_URI))
+						.build())
+				.and(route("add_new_patient")
+						.POST("/patient", http())
+						.before(uri(PATH_PATIENT_URI))
+						.build())
+				.and(route("patient_delete")
+					.DELETE("/patient/delete/{id}", http())
+					.before(uri(PATH_PATIENT_URI))
+					.build()
+		);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedilaboApplication.class, args);
+	}
+
+}
---
+++ medilabopatient/src/main/java/com/ocab/medilabopatient/model/dto/request/t.java
@@ -0,0 +1,4 @@
+package com.ocab.medilabopatient.model.dto.request;
+
+public class t {
+}
