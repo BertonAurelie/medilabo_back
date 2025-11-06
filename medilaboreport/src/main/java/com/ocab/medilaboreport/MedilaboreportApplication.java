5ace180cd61db3c21aad54e76091d3054660c2ad
AB <ab>
2025-11-06 12:04:06 +0100

    add microservice medilabo report

---
+++ medilaboreport/src/main/java/com/ocab/medilaboreport/MedilaboreportApplication.java
@@ -0,0 +1,13 @@
+package com.ocab.medilaboreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedilaboreportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedilaboreportApplication.class, args);
+	}
+
+}
